d6365565eb21177652fdb37f69328c1672a5afba
Tristan_yueng <5915762@qq.com>
2017-05-01 15:23:55 +0800

    demo

---
+++ GCDCancellerDemo/GCDCancellerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDCancellerDemo
+//
+//  Created by Tristan on 2017/4/27.
+//  Copyright © 2017年 Tristan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
